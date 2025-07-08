39886eacbccbcdb4bfb279a24f3fb14f67f69b9a
wlals <jiminelp@gmail.com>
2025-07-08 12:27:30 +0900

    feat :: cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,14 @@ import { participationRouter } from "./routes/participation.routes";
 
 const app: Application = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:3001",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/auth", authRouter);
